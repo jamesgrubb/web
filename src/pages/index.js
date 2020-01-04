b90590657f0ea61d2918bc6828ecae3af2677c81
jamesgrubb <jamesgrubb@mac.com>
2020-01-04 17:45:14 +0000

    gatsby-styled-components plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,37 @@ import Test from '../components/Tests';
 import MakePDF from '../components/MakePDF'
 export default () => {
     
+const makePdf = (e) => {
+  e.preventDefault();
+  
+  
+  const pageToScreenshot = "http://localhost:8000"
+  const options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json; charset=utf-8" },
+    body: JSON.stringify({ pageToScreenshot: pageToScreenshot })
+};
+fetch("/.netlify/functions/take-screenshot", options)
+.then((res) => res.json())
+.then((res) => {
+    
+    if (!res.pdf) return ;
 
+    // const img = document.createElement('img');
+    // img.src = bufferToImageUrl(res.buffer.data);
+    // document.getElementById('result').innerHTML = img.outerHTML;
+})
+.catch((err) => {
+    console.log(err)
+});
+}
     return(
    <ThemeProvider theme={theme}>
    <>
    <GlobalStyles/>
+   <button handleClick={makePdf}>makePDF</button>
     <Layout >
-      <MakePDF />
+      
     
     <Test />
     </Layout>
